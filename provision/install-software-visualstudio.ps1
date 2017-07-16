b9bb8ff823e7d4edaf93b93eb643d4ce1423bbad
Morten Larsen <baunegaard@live.dk>
2017-07-16 20:12:38 +0200

    Make sure VS install waits

--- provision/install-software-visualstudio.ps1
+++ provision/install-software-visualstudio.ps1
@@ -1,2 +1,2 @@
 Write-Host "Installing Visual Studio 2017. This may take a while..."
-C:\vagrant\provision\vs_community__1307844785.1500130882.exe --wait --passive --norestart --all
+Start-Process -FilePath 'C:\vagrant\provision\vs_community__1307844785.1500130882.exe' -ArgumentList ('--wait','--passive','--norestart','--all') -Wait
