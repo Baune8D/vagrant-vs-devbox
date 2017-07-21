05a791ef06e24587016317ddd6d54a92ab91d4de
Morten Larsen <baunegaard@live.dk>
2017-07-21 13:25:52 +0200

    Open firewall ports

--- provision/configure-post-windowssettings.ps1
+++ provision/configure-post-windowssettings.ps1
@@ -1,8 +1,12 @@
+Write-Host "Adding firewall rules"
+netsh advfirewall firewall add rule name="Conveyor" dir=in action=allow protocol=TCP localport=45455
+netsh advfirewall firewall add rule name="Webpack-Dev-Server" dir=in action=allow protocol=TCP localport=9000
+
 New-Item ~\.ssh -Type Directory
 Copy-Item C:\vagrant\ssh\bashrc -Destination ~\.bashrc
 
 Install-BoxstarterPackage -PackageName ~\boxstarter\boxstarter-post-windowssettings.txt -DisableReboots
 
-# Cleanup
+Write-Host "Cleaning up"
 Remove-Item ~\boxstarter -Recurse -Force
 Remove-Item C:\tmp -Recurse -Force
