6c50849bb5a66161756c0cfbdb2caf356913697b
Morten Larsen <baunegaard@live.dk>
2018-08-15 11:35:50 +0200

    Fix install of Visual Studio

--- software/install-visualstudio.ps1
+++ software/install-visualstudio.ps1
@@ -1,3 +1,3 @@
 Write-Host "Installing Visual Studio 2017. This may take a while..."
-$exe = Get-ChildItem vs_*.exe | Select-Object Name
+$exe = Get-ChildItem -Path C:\vagrant\software\vs_*.exe -File | Select-Object Name
 Start-Process -FilePath "C:\vagrant\software\$($exe.Name)" -ArgumentList ('--wait','--passive','--norestart','--all') -Wait
